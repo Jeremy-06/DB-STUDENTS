4b27537ab9d2d74232ebc5c32a492eb884cdf2d8
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-06-15 15:22:27 +0800

    ADD MODULE

---
+++ Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Module1
+
+    Public con As MySqlConnection = New MySqlConnection("server=localhost;user id=root;password=;database=db_students;port=3306;")
+    Public dataAdapter As MySqlDataAdapter
+    Public dataReader As MySqlDataReader
+    Public command As MySqlCommand = New MySqlCommand()
+    Public datSet As DataSet = New DataSet()
+    Public dataTable As DataTable = New DataTable()
+    Public query As String
+
+End Module
