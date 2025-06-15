a189e7f8af3ceee058cacd3a4d7c40473b3271a6
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-06-15 12:38:41 +0800

    CONN OPEN

--- Form1.vb
+++ Form1.vb
@@ -1,1 +1,28 @@
-﻿
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Public con As MySqlConnection = New MySqlConnection("server=localhost;user id=root;password=;database=db_students;port=3306;")
+    Public datSet As DataSet = New DataSet()
+    Public dataAdapter As MySqlDataAdapter
+    Public dataTable As DataTable = New DataTable()
+    Public dataReader As MySqlDataReader
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+    Public Sub LoadData()
+        Try
+            con.Open()
+            Dim query As String = "SELECT * FROM students_profile"
+            dataAdapter = New MySqlDataAdapter(query, con)
+            dataTable.Clear()
+            dataAdapter.Fill(dataTable)
+            DataGridView1.DataSource = dataTable
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+End Class
