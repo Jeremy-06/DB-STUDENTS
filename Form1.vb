4a9ee40be672df5099242101b1f2b263c5c68cc4
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-06-15 15:26:18 +0800

    INSERT

--- Form1.vb
+++ Form1.vb
@@ -1,18 +1,13 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
-    Public con As MySqlConnection = New MySqlConnection("server=localhost;user id=root;password=;database=db_students;port=3306;")
-    Public datSet As DataSet = New DataSet()
-    Public dataAdapter As MySqlDataAdapter
-    Public dataTable As DataTable = New DataTable()
-    Public dataReader As MySqlDataReader
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadData()
     End Sub
+    ' load data from database
     Public Sub LoadData()
         Try
-            con.Open()
             Dim query As String = "SELECT * FROM students_profile"
             dataAdapter = New MySqlDataAdapter(query, con)
             dataTable.Clear()
@@ -25,4 +20,42 @@ Public Class Form1
         End Try
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim student_id = TextBox1.Text.Trim()
+        Dim first_name = TextBox2.Text.Trim()
+        Dim last_name = TextBox3.Text.Trim()
+        Dim age = TextBox4.Text.Trim()
+        Dim address = TextBox6.Text.Trim()
+        Dim birthday = DateTimePicker1.Value.ToString("yyyy-MM-dd")
+
+        ' Check for empty fields
+        If String.IsNullOrWhiteSpace(first_name) OrElse
+           String.IsNullOrWhiteSpace(last_name) OrElse
+           String.IsNullOrWhiteSpace(age) OrElse
+           String.IsNullOrWhiteSpace(address) Then
+            MessageBox.Show("All fields must be filled out.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Try
+            con.Open()
+            query = "INSERT INTO students_profile (student_id, first_name, last_name, age, address, birthday) VALUES (@student_id, @first_name, @last_name, @age, @address, @birthday)"
+            command = New MySqlCommand(query, con)
+
+            command.Parameters.AddWithValue("@student_id", student_id)
+            command.Parameters.AddWithValue("@first_name", first_name)
+            command.Parameters.AddWithValue("@last_name", last_name)
+            command.Parameters.AddWithValue("@age", age)
+            command.Parameters.AddWithValue("@address", address)
+            command.Parameters.AddWithValue("@birthday", birthday)
+            command.ExecuteNonQuery()
+            MessageBox.Show("Data inserted successfully!")
+            LoadData()
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
 End Class
